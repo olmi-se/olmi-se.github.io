ba7ba2c7712ecfe8a8d326d7af10eafe36f74115
oleg-gumeniuk <oleg.gumeniuk@proposalsfactory.com>
2019-10-04 13:23:33 +0200

    made the website responsive, edited the texts, minor improvements

--- js/main.js
+++ js/main.js
@@ -3,35 +3,13 @@ $(document).ready(function(){
 
 	$("#portfolio-contant-active").mixItUp();
 
-
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
-	});
-
-
-
-
 	$('#clients-logo').owlCarousel({
 		autoPlay: 3000,
 	    loop:true,
 	    margin:10,
 	    center:true,
-	    items: 3,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        400:{
-	            items:2
-	        },
-	        600:{
-	            items:3
-	        }
-	    }
+	    items:3
 	})
-
 
 	// google map
 		var map;
